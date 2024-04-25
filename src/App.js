0063e9cc3548dfa1e901c14997c6fe5ea41532b9
Colin <colindcirian@gmail.com>
2024-04-25 10:10:35 -0700

    added tail wind and components

--- src/App.js
+++ src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import About from './pages/About';
-import Contact from './pages/Contact';
 
 export default function App() {
   return (
@@ -12,7 +11,6 @@ export default function App() {
           <Route index element= {<Home />}></Route>
           <Route path = "/home" element = {<Home />}></Route>
           <Route path = "/about" element = {<About />}></Route>
-          <Route path = "/contact" element = {<Contact />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,13 @@
 import React from "react";
+import "../styles/Home.css";
+import "../components/Buttons";
 
 export default function Home() {
     return (
-        <h1>Home Page</h1>
+        <div>
+            <h1>this is lauryns website and its ugly right now but give me some time</h1>
+
+        </div>
+       
     )
 }
